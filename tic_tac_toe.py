3d5a11c1e3ab1f7a9482c01bfcdc0765f414b75f
Dmytriy Myk <porosya@gmail.com>
2018-07-05 06:39:18 +0300

    check_board, draw_board

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,24 +1,32 @@
 def draw_board(board):
     print(" ", " ".join("123"))
-    for i, row in enumerate(board):
-        print(i+1, *row)
-    # print("\n".join([" ".join([x for x in y]) for y in board_print]))
+    print("\n".join([" ".join([x for x in y]) for y in list(zip("123", *board))]))
 
 
 def game_init():
-    return [["." for x in range(3)] for y in range(3)]
+    return [["." for x in range(3)] for y in range(3)], "O"
 
 
 def add_board(board, coords, player):
     x, y = [int(_) - 1 for _ in coords]
-    board[x][y] = player
+    if board[x][y] == ".":
+        board[x][y] = player
+    else:
+        print("\nЗанято !!!\n")
 
 
-def check_board(board):
-    pass
+def check_board(board, player):
+    win = 0
+    res = 0
+    for pl in ["X", "O"]:
+        print(pl)
+        if any(x == pl for x in [column for column in board]):
+            print("found", pl)
+
+    return win, res, player
 
 
-def valide_input(inp):
+def validate_input(inp):
     return inp.isdigit() and 0 < int(inp) < 4
 
 
@@ -28,19 +36,20 @@ def input_board(board, player):
 
     while not x and not y:
         xt, yt = input(f"Игрок {player} введите координаты через запятую :").split(",")
-        if valide_input(xt) and valide_input(yt):
+        if validate_input(xt) and validate_input(yt):
             x, y = xt, yt
         else:
-            print("Что то ты ввел неправильно .... Давай еще разок ")
+            print("Что-то ты ввел неправильно .... Давай еще разок ")
     return add_board(board, (x, y), player)
 
 
 def main():
-    board = game_init()
+    board, player = game_init()
     while True:
         # print(board)
         draw_board(board)
-        input_board(board, "O")
+        input_board(board, player)
+        win, res, player = check_board(board, player)
 
 
 if __name__ == "__main__":
